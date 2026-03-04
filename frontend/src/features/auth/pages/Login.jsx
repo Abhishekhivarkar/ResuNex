e021d783938586f0a9903db695e3bd84032b8b4b
Abhishekhivarkar <abhihivarkar783@gmail.com>
2026-03-04 11:29:55 +0530

    feat(auth): implement login and register pages with custom styled form

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,33 @@
-export const Login = () =>{
- return(
-  <>
-   <div>Login Page</div>
-  </>
+import "../auth.form.scss"
+export const Login = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <>
+      <main>
+        <div className="form-container">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input name="identifier" type="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" placeholder="Enter password" name="password" id="password" />
+            </div>
+            <div>
+              <button type="submit" className="button primary-button">
+                <div className="top">Login</div>
+                <div className="bottom"></div>
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
   )
 }
 
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,9 +1,39 @@
-export const Register = () =>{
- return(
-  <>
-   <div>
-   Register Page</div>
-  </>
+export const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+  return (
+
+
+    <>
+      <main>
+        <div className="form-container">
+          <h1>Register</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input name="userName" type="text" id="username" placeholder="Enter your username" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input name="email" type="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" placeholder="Enter password" name="password" id="password" />
+            </div>
+            <div>
+              <button type="submit" className="button primary-button">
+                <div className="top">Register</div>
+                <div className="bottom"></div>
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
   )
 }
 
