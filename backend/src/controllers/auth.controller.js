a99dea620ac76565d931657c00996df470adc856
Abhishekhivarkar <abhihivarkar783@gmail.com>
2026-03-10 16:29:27 +0530

    bug fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -87,12 +87,12 @@ export const  login = async (req, res) => {
 
     const token = generateToken(user)
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    })
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+})
 
     res.status(200).json({
       message: " successful",
